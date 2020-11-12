0d80d10ccf5ff6834c0b2b1f6a25711ff772de3d
partypanda5 <scoondi@hotmail.com>
2020-11-12 15:25:47 +0200

    layout adjustments

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 var section = $('li');
-var Chennai = document.getElementById('Chennai');
-var Dhaka = document.getElementById('Dhaka');
-var Yangon = document.getElementById('Yangon');
-var London = document.getElementById('London');
-var sectionOne = document.getElementById('sectionOne');
-var sectionTwo = document.getElementById('sectionTwo');
-var sectionThree = document.getElementById('sectionThree');
-var sectionFour = document.getElementById('sectionFour');
 
 function toggleAccordion() 
 {
@@ -15,35 +7,3 @@ function toggleAccordion()
 }
 
 section.on('click', toggleAccordion);
-
-Chennai.addEventListener('click', () => 
-{
-  sectionTwo.classList.remove('active');
-  sectionThree.classList.remove('active');
-  sectionFour.classList.remove('active');
-  sectionOne.classList.add('active');
-});
-
-Dhaka.addEventListener('click', () => 
-{
-  sectionTwo.classList.add('active');
-  sectionThree.classList.remove('active');
-  sectionFour.classList.remove('active');
-  sectionOne.classList.remove('active');
-});
-
-Yangon.addEventListener('click', () => 
-{
-  sectionTwo.classList.remove('active');
-  sectionThree.classList.add('active');
-  sectionFour.classList.remove('active');
-  sectionOne.classList.remove('active');
-});
-
-London.addEventListener('click', () => 
-{
-  sectionTwo.classList.remove('active');
-  sectionThree.classList.remove('active');
-  sectionFour.classList.add('active');
-  sectionOne.classList.remove('active');
-});
