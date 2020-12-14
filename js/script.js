7420b85a53cb8c6d6e9ee73c3994a57a5a606bf8
partypanda5 <scoondi@hotmail.com>
2020-12-14 10:15:41 +0200

    scrolling content

--- js/script.js
+++ js/script.js
@@ -22,10 +22,19 @@ var subtitleContainerFour = document.getElementById('subtitleContainerFour');
 var focusedSubtitlesFour = document.getElementById('focusedSubtitlesFour');
 
 var dhakaScroll = false;
-var scrollCounter = 0;
 
 var videoContent = document.getElementById('videoContent');
 var imageContent = document.getElementById('imageContent');
+var territorialDisruptionImage1 = document.querySelector("#TerritorialDisruptionImage1");
+var territorialDisruptionImage2 = document.querySelector("#TerritorialDisruptionImage2");
+var territorialDisruptionImage3 = document.querySelector("#TerritorialDisruptionImage3");
+var territorialDisruptionImage4 = document.querySelector("#TerritorialDisruptionImage4");
+var territorialDisruptionImage5 = document.querySelector("#TerritorialDisruptionImage5");
+var territorialDisruptionImage6 = document.querySelector("#TerritorialDisruptionImage6");
+var territorialDisruptionImage7 = document.querySelector("#TerritorialDisruptionImage7");
+var territorialDisruptionImage8 = document.querySelector("#TerritorialDisruptionImage8");
+var territorialDisruptionImage9 = document.querySelector("#TerritorialDisruptionImage9");
+var territorialDisruptionVideo = document.querySelector("#TerritorialDisruptionVideo");
 
 var section = $('li');
 
@@ -87,6 +96,7 @@ DhakaTitle.addEventListener('click', () =>
 {
   cityDhaka.style.fontWeight = '900';
   dhakaScroll = true;
+  var scrollCounter = 0;
 
   var changeDhakaPosition = setTimeout(dhakaTimer, 1000);
 
@@ -109,62 +119,137 @@ DhakaTitle.addEventListener('click', () =>
   $("#cityYangon").delay(250).animate({"opacity": "0"}, 500);
   $("#cityLondon").delay(250).animate({"opacity": "0"}, 500);
 
-  if (dhakaScroll === true && scrollCounter == 0) 
-  {
-    primaryPanelContainer.addEventListener('wheel', () => 
+  // -----------------------------------FIRST SCROLL-------------------------------------------------
+
+    primaryPanelContainer.addEventListener('wheel', (e) => 
     {
-      $("#primaryPanel").delay(250).animate({"opacity": "0"}, 600);
-      $("#videoContent").delay(250).animate({"opacity": "0"}, 600);
-      $("#cityDhaka").delay(100).animate({"opacity": "0"}, 600);
-      $("#imageContent").delay(100).animate({"opacity": "1"}, 600);
-
-      primaryPanelContainer.innerHTML = `<div id='cityHeadings'><a href='#'><h1 id='hydroTitle' class='cityName'>Hydrological <br/>Infrastructure</h1></a></div><div id='primaryPanel' class='hydrologicalContentText'><p class='contentText'>Since the late 1980s, Dhaka East has been the target of speculative urban development driven by the powerful real estate sector. It is an area of low floodplains and marshy lands that are inundated during the monsoon. To prepare land for development, land is elevated above predicted flood levels through a process known as sand pumping. Suction dredgers extract vast quantities of sand  from river beds, which is then pumped from barges via steel pipelines into the floodplains and marshlands. Dhaka East is this being reimagined and reformed from the fluid fabric of its surroundings through the pumping and piping of sand.</p><a id='explore' href='#'>Explore Map</a></div>`;
-
-      var hydrologicalContentText = document.querySelector('.hydrologicalContentText');
-
-      hydrologicalContentText.style.top = "25vh";
-      contentSectionTitle.style.top = "8vh";
-
-      var exploreButton = document.getElementById('explore');
-      var backToHydro = document.getElementById('backHyrdo');
-
-      exploreButton.addEventListener('click', () => 
+      if (dhakaScroll === true && scrollCounter === 0) 
       {
-          $("#primaryPanel").delay(250).animate({"opacity": "0"}, 500);
-          $(".cityTitle").delay(250).animate({"opacity": "0"}, 500);
-          $(".cityName").delay(250).animate({"opacity": "0"}, 500);
-          $("#imageContent").delay(250).animate({"z-index": "5"}, 500);
-          $("#backHyrdo").delay(250).animate({"opacity": "1"}, 500);
-      });
-
-      backToHydro.addEventListener('click', () => 
-      {
-          $("#imageContent").delay(250).animate({"z-index": "0"}, 100);
-          $("#primaryPanel").delay(250).animate({"opacity": "1"}, 500);
-          $(".cityTitle").delay(250).animate({"opacity": "1"}, 500);
-          $(".cityName").delay(250).animate({"opacity": "1"}, 500);
-          $("#backHyrdo").delay(250).animate({"opacity": "0"}, 500);
-      });
-
-      console.log(scrollCounter, dhakaScroll);
-      scrollCounter = 1;
-
-    if (dhakaScroll === true && scrollCounter == 1)
-    {
-      primaryPanelContainer.addEventListener('wheel', () => 
-      {
-        scrollCounter = 2;
-        console.log(scrollCounter, dhakaScroll);
+        e.preventDefault();
         $("#primaryPanel").delay(250).animate({"opacity": "0"}, 600);
-        $("#imageContent").delay(100).animate({"opacity": "0"}, 600);
-        $(".cityTitle").delay(250).animate({"opacity": "0"}, 500);
-        $(".cityName").delay(250).animate({"opacity": "0"}, 500);
-      });
-    } 
+        $("#videoContent").delay(250).animate({"opacity": "0"}, 600);
+        $("#cityDhaka").delay(100).animate({"opacity": "0"}, 600);
+        $("#imageContent").delay(100).animate({"opacity": "1"}, 600);
+
+        primaryPanelContainer.innerHTML = `<div id='cityHeadings'><a href='#'><h1 id='hydroTitle' class='cityName ChapterName'>Hydrological Infrastructure</h1></a></div><div id='primaryPanel' class='hydrologicalContentText'><p class='contentText'>Since the late 1980s, Dhaka East has been the target of speculative urban development driven by the powerful real estate sector. It is an area of low floodplains and marshy lands that are inundated during the monsoon. To prepare land for development, land is elevated above predicted flood levels through a process known as sand pumping. Suction dredgers extract vast quantities of sand  from river beds, which is then pumped from barges via steel pipelines into the floodplains and marshlands. Dhaka East is this being reimagined and reformed from the fluid fabric of its surroundings through the pumping and piping of sand.</p><a id='explore' href='#'>Explore Map</a></div>`;
+
+        var hydrologicalContentText = document.querySelector('.hydrologicalContentText');
+
+        hydrologicalContentText.style.top = "25vh";
+        contentSectionTitle.style.top = "8vh";
+
+        var exploreButton = document.getElementById('explore');
+        var backToHydro = document.getElementById('backHyrdo');
+
+        exploreButton.addEventListener('click', () => 
+        {
+            $("#primaryPanel").delay(250).animate({"opacity": "0"}, 500);
+            $(".cityTitle").delay(250).animate({"opacity": "0"}, 500);
+            $(".cityName").delay(250).animate({"opacity": "0"}, 500);
+            $("#imageContent").delay(250).animate({"z-index": "5"}, 500);
+            $("#backHyrdo").delay(250).animate({"opacity": "1"}, 500);
+            dhakaScroll = false;
+        });
+
+        backToHydro.addEventListener('click', () => 
+        {
+            $("#imageContent").delay(250).animate({"z-index": "0"}, 100);
+            $("#primaryPanel").delay(250).animate({"opacity": "1"}, 500);
+            $(".cityTitle").delay(250).animate({"opacity": "1"}, 500);
+            $(".cityName").delay(250).animate({"opacity": "1"}, 500);
+            $("#backHyrdo").delay(250).animate({"opacity": "0"}, 500);
+            dhakaScroll = true;
+        });
+        
+        scrollCounter ++;
+    }
+
+       // -----------------------------------SECOND SCROLL-------------------------------------------
+
+    else if (dhakaScroll === true && scrollCounter === 1)
+    {
+        e.preventDefault();
+        $("#imageContent").delay(0).animate({"z-index": "0"}, 100);
+        imageContent.style.display ="none";
+        $("#primaryPanel").delay(0).animate({"opacity": "0"}, 500);
+        $("#cityHeadings").delay(0).animate({"opacity": "0"}, 500);
+
+        primaryPanelContainer.innerHTML =
+        `
+          <div id="cityHeadings">
+            <a href="#"><h1 class="ChapterName">Territorial Disruption</h1></a>
+          </div>
+          <div id="TerritorialDisruptionPrimaryPanel" class="content">
+              <p class="TerritorialDisruptionText">The forcefulness of this vitality carries with it the
+                potential for violent territorial disruption. Dwelling
+                alongside mobile sediments require tuning to
+                and working with their fluid becomings, while
+                acknowledging their fearsome capacity to undo
+                sustaining human connections and footings.</p>
+          </div>
+      </div>
+        `;
+
+        $("#TerritorialDisruptionVideo").delay(250).animate({"opacity": "1"}, 500);
+        $("#TerritorialDisruptionImage1").delay(250).animate({"opacity": "1"}, 500);
+        $("#TerritorialDisruptionImage2").delay(250).animate({"opacity": "1"}, 500);
+        $("#TerritorialDisruptionImage3").delay(250).animate({"opacity": "1"}, 500);
+
+        scrollCounter ++;
+    }
+
+         // -----------------------------------THIRD SCROLL-----------------------------------------
+
+        else if (dhakaScroll === true && scrollCounter === 2)
+        {
+            e.preventDefault();
+            $("#primaryPanel").delay(250).animate({"opacity": "0"}, 100);
+            $("#cityHeadings").delay(250).animate({"opacity": "0"}, 100); 
+            $("#TerritorialDisruptionVideo").delay(0).animate({"opacity": "0"}, 300);
+            $("#TerritorialDisruptionImage1").delay(0).animate({"opacity": "0"}, 300);
+            $("#TerritorialDisruptionImage2").delay(0).animate({"opacity": "0"}, 300);
+            $("#TerritorialDisruptionImage3").delay(0).animate({"opacity": "0"}, 300);
+
+            territorialDisruptionImage1.style.display = "none";
+            territorialDisruptionImage2.style.display = "none";
+            territorialDisruptionImage3.style.display = "none";
+            territorialDisruptionVideo.style.display = "none";
+    
+            primaryPanelContainer.innerHTML =
+            `
+            `;
+
+            $("#TerritorialDisruptionImage4").delay(1000).animate({"opacity": "1"}, 300);
+            $("#TerritorialDisruptionImage5").delay(3500).animate({"opacity": "1"}, 300);
+            $("#TerritorialDisruptionImage6").delay(2000).animate({"opacity": "1"}, 300);
+            $("#TerritorialDisruptionImage7").delay(3000).animate({"opacity": "1"}, 300);
+            $("#TerritorialDisruptionImage8").delay(2500).animate({"opacity": "1"}, 300);
+            $("#TerritorialDisruptionImage9").delay(500).animate({"opacity": "1"}, 300);
+    
+            scrollCounter ++;
+            console.log(scrollCounter, dhakaScroll);
+          }
+
+         else if (dhakaScroll === true && scrollCounter === 3)
+          {
+              e.preventDefault();      
+ 
+              $("#TerritorialDisruptionImage4").fadeOut(1000);
+              $("#TerritorialDisruptionImage5").fadeOut(1000);
+              $("#TerritorialDisruptionImage6").fadeOut(1000);
+              $("#TerritorialDisruptionImage7").fadeOut(1000);
+              $("#TerritorialDisruptionImage8").fadeOut(1000);
+              $("#TerritorialDisruptionImage9").fadeOut(1000);
+
+              $("#blueLinesOne").fadeIn(2000);
+      
+              scrollCounter ++;
+          } 
+
+    });
   });
-  }
 });
-});
+
 
 var zoom = new dmuka.Zoom({
   element: document.querySelector("#hydrologicalImage"),
